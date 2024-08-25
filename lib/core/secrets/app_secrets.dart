239fcbbfb67a561afa1d2d9ce9f1be5042c5cb1f
malachieborohoul <malachieborohoul@gmail.com>
2024-08-25 12:41:49 +0100

    Remote datasource and Repository implementation

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://elkhuoiamkyfaqwjnrbk.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsa2h1b2lhbWt5ZmFxd2pucmJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ1NzY3ODAsImV4cCI6MjA0MDE1Mjc4MH0.zH1gPqTVYb3A7d0vxO9oted99fUGZ32nmwzP5-FMUuc';
+}
---
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,32 @@
+import 'package:blog_app/core/error/exceptions.dart';
+import 'package:blog_app/core/error/failures.dart';
+import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/domain/repositories/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource authRemoteDataSource;
+
+  AuthRepositoryImpl(this.authRemoteDataSource);
+  @override
+  Future<Either<Failure, String>> signIn(
+      {required String email, required String password}) {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, String>> signUp(
+      {required String name,
+      required String email,
+      required String password}) async {
+    try {
+      final userId = await authRemoteDataSource.signUp(
+          name: name, email: email, password: password);
+
+      return right(userId);
+    } on ServerException catch (e) {
+      return left(Failure(e.message));
+    }
+  }
+}
